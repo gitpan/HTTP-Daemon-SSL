8f9089f671ff64da26cb473e82e6e436c5a7b6e3
Peter Behroozi <behroozi@www.pls.uni.edu>
2003-07-27 16:23:12 +0000

    Import of BEHROOZI/HTTP-Daemon-SSL-1.01 from CPAN.
    gitpan-cpan-distribution: HTTP-Daemon-SSL
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         BEHROOZI/HTTP-Daemon-SSL-1.01.tar.gz
    gitpan-cpan-author:       BEHROOZI
    gitpan-cpan-maturity:     released

--- SSL.pm
+++ SSL.pm
@@ -69,7 +69,7 @@ use vars qw($VERSION @ISA $PROTO $DEBUG);
 use IO::Socket::SSL;
 use HTTP::Daemon;
 
-$VERSION = "1.00";
+$VERSION = "1.01";
 @ISA = qw(IO::Socket::SSL HTTP::Daemon);
 
 =item $d = new HTTP::Daemon::SSL
@@ -128,8 +128,37 @@ sub ssl_error {
 
 
 package HTTP::Daemon::ClientConn::SSL;
-use vars qw(@ISA);
+use vars qw(@ISA $DEBUG);
 @ISA = qw(IO::Socket::SSL HTTP::Daemon::ClientConn);
+*DEBUG = \$HTTP::Daemon::DEBUG;
+
+sub _need_more
+{
+    my $self = shift;
+    if ($_[1]) {
+        my($timeout, $fdset) = @_[1,2];
+        print STDERR "select(,,,$timeout)\n" if $DEBUG;
+        my $n = select($fdset,undef,undef,$timeout);
+        unless ($n) {
+            $self->reason(defined($n) ? "Timeout" : "select: $!");
+            return;
+        }
+    }
+    my $total = 0;
+    while (1){
+        print STDERR sprintf("sysread() already %d\n",$total) if $DEBUG;
+        my $n = sysread($self, $_[0], 2048, length($_[0]));
+        print STDERR sprintf("sysread() just \$n=%s\n",(defined $n?$n:'undef')) if $DEBUG;
+        $total += $n if defined $n;
+        last if $! =~ 'Resource temporarily unavailable';
+            #SSL_Error because of aggressive reading
+
+        $self->reason(defined($n) ? "Client closed" : "sysread: $!") unless $n;
+        last unless $n;
+        last unless $n == 2048;
+    }
+    $total;
+}
 
 =head1 SEE ALSO
 
